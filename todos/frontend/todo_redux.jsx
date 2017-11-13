c3a389d88d397dba7adf07ae551c86b3548e7362
jeffren716 <jeffyiren@gmail.com>
2017-11-13 15:08:09 -0800

    finish root of phase 3

--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { receiveTodo, receiveTodos } from './actions/todo_actions';
+import Root from './components/root.jsx';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hi!</h1>, root);
+  const store = configureStore();
+  //kill the next line before production
+  window.store = store;
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
+  ReactDOM.render(<Root store={ store }/>, root);
 });
